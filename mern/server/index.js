1aa21824f92958ed8a0d60a95d9be0d001924cdf
helwyza <helwyza@gmail.com>
2024-10-07 16:04:57 +0700

    initiate dev branch

--- mern/server/index.js
+++ mern/server/index.js
@@ -9,7 +9,14 @@ dotenv.config({});
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "https://job-fair-client.vercel.app", // Ganti dengan URL front-end Anda
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // Izinkan pengiriman cookie atau authorization header jika diperlukan
+  })
+);
 app.use(express.json());
 app.use("/record", records);
 
